ca459e65b667b12714417939cf2bfbd151e14573
Sujay Balamurugan <93249134+superking816pro@users.noreply.github.com>
2023-06-19 15:39:26 +0530

    tests - debuging pending

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import webbrowser
 import time
+import os
+import json
+import requests
+
+path = "/temp"
+isExist = os.path.exists(path)
+if isExist == False:
+    os.mkdir("temp")
 
 username = input("enter the sepcific username to see the info: ")
 inputURL = 'https://api.github.com/users/' + username
@@ -7,20 +15,41 @@ openwebbrowser = webbrowser.open(inputURL, new=2)
 time.sleep(2)
 repoornot = input("do you want to see the information about their repositories(y/n): ")
 
+request1 = requests.get(inputURL, allow_redirects=True)
 
-def choose():
-    if repoornot == "y" or "Y":
-        InputURL = 'https://api.github.com/users/' + username + '/repos'
-        openwebbrowser = webbrowser.open(InputURL, new=2)
-        time.sleep(10)
+with open('temp/superking816pro.json','r') as firstfile:
+    for line in firstfile:
+        jsonfile = line
 
-    if repoornot == "n" or "N":
-        print("terminating process")
-        time.sleep(5)
+loadjson = json.loads(jsonfile)
 
-    else:
-        print("enter y or n only")
-        choose()
+print("username:" + jsonfile["login"])
+print("type:" + jsonfile["type"])
+print("site admin:" + jsonfile["site_admin"])
+print("name:" + jsonfile["name"])
+print("company:" + jsonfile["company"])
+print("blog:" + jsonfile["blog"])
+print("location:" + jsonfile["location"])
+print("email:" + jsonfile["email"])
+print("hireable:" + jsonfile["hireable"])
+print("bio:" + jsonfile["bio"])
+print("twitter username:" + jsonfile["twitter_username"])
+print("public repos:" + jsonfile["public_repos"])
+print("public gists:" + jsonfile["public_gists"])
+print("followers:" + jsonfile["followers"])
+print("following:" + jsonfile["following"])
+print("created at:" + jsonfile["created_at"])
+print("updated at:" + jsonfile["updated_at"])
 
 
-choose()
+if repoornot == "y" or "Y":
+    InputURL = 'https://api.github.com/users/' + username + '/repos'
+    openwebbrowser = webbrowser.open(InputURL, new=2)
+    time.sleep(10)
+
+if repoornot == "n" or "N":
+    print("terminating process")
+    quit()
+
+else:
+    print("enter y or n only")
